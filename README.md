274a68ec24173d7dc1e7f71b365d471168b441ce
Sergi Reyner <sergi.reyner@gmail.com>
2019-04-16 20:59:23 +0100

    Initial commit
    Working conveyors, ramps, furnaces, walls, and empty space

---
+++ README.md
@@ -0,0 +1,57 @@
+# RubE On Conveyor Belts
+  **RubE On Conveyor Belts** (abbreviated to **ROCB** in this article
+  and pronounced _"Roob-Ee On Conveyor Belts"_, a bad pun on _"Ruby on
+  Rails"_) is a language created in 2007 by @immibis, and based on
+  Chris Pressey's @RUBE.
+
+## File format
+
+  The file format is the grid itself.
+
+## The control program
+
+  The default control program is
+  
+    +[dsti[o[-]]+]
+
+## The main program
+
+
+| dds  | dsds | dsds |
+|------|------|------|
+| dsds | dsds | dsds |
+
+
+### Loading
+
+  Loading happens in X phases:
+  
+  1. The text file is loaded, lines are padded/trimmed to 80 characters
+  2. If the file is longer than 25 lines, additional lines are
+     ignored. If it's shorter, additional 80-character blank lines are
+     added until there's 25.
+  3. 
+
+### Order of evaluation
+  Every part in the main program performs its action, in the following
+  categories - lower category numbers perform their actions first, and
+  from left to right, bottom to top, within a category.
+
+  0. output (*), input(?), splitter(separator), state control
+  1. door key(actuate the door), furnace, random crate(generate random)
+  2. up pipe, down pipe
+  3. data/crates (??), ramps (???)
+  4. gravity(global, not a part), copiers, winches
+  5. dozers, conveyors (the ones to the right first, each type together, in order)
+  6. gate(sorter), adder, subtracter
+
+Evaluation happens in frames, and should be double buffered, that is:
+
+    The results of the actions of a part will not affect the results
+    of the activation of parts activated later in the same frame, and
+    at the end of the frame, the output becomes input and a new frame
+    is created as output.
+
+There's a data layer and a code layer. That's because data may occupy
+the same place as code.
+
---
+++ rube.rb
@@ -0,0 +1,603 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# interpreter for RubE On Conveyor Belts
+# https://esolangs.org/wiki/RubE_On_Conveyor_Belts
+
+# raydeejay (2019/04/16 at 14:33)
+#   ruby 
+#   I'm implementing RubE (on Conveyor Belts) on Ruby (on Rails)
+#   (only I'm not actually using Rails, but I'm upping the pun factor here)
+
+require 'singleton'
+
+# Utility code
+##############################
+def moveto(x, y)
+  print "\x1B[#{y+1};#{x+1}H"
+end
+
+def clear()
+  print "\x1B[2J"
+end
+
+def colorForChar(c)
+  {
+    "=" => "33",
+    "-" => "31",
+    "+" => "32",
+    "<" => "37;1",
+    ">" => "37;1",
+    "(" => "33;1",
+    ")" => "33;1",
+    "#" => "31;1",
+    "\"" => "36;1",
+    "[" => "35;1",
+  }[c] or "0"
+end
+
+def printCode(code, x, y)
+  moveto(x, y)
+  print "\x1B[0m "
+  moveto(x, y)
+  print "\x1B[#{colorForChar(code)}m#{code}\x1B[0m"
+end
+
+
+################################
+# Loading the grid
+if ARGV.length == 1 then
+  s = File.new(ARGV[0], "r")
+else
+  print "no program\n"
+  exit 1
+end
+
+code = s.collect { |line| line }
+s.close()
+
+# Global-ish vars, need refactor
+################################
+vars = [0]
+varl = varp = 0
+posx = posy = 0
+oldx = oldy = 0
+dirx = 1
+diry = 0
+elevator = false
+skip = 0
+$output = ""
+$outputCount = 20
+visual = (ARGV.length == 1)
+$delay = 1
+prefix = 0
+should_collect = false
+
+$dataGrid = Array.new(25) { Array.new(80) { $empty } }
+$codeGrid = Array.new(25) { Array.new(80) { $empty } }
+
+$futureDataGrid = $dataGrid.map do |each| each.dup end
+$futureCodeGrid = $codeGrid.map do |each| each.dup end
+
+$dirty = []
+
+# Independent-ish code
+##############################
+def pushBlocksLeft(x, y)
+  # can't push if outside the grid
+  return false if y < 0 or y >= 25 or x < 0 or x >= 80
+
+  pos_left_edge = x
+  pos_left_edge -= 1 while $codeGrid[y][pos_left_edge].crate? and pos_left_edge > 0
+
+  # can't push into the edge of the program
+  return false if pos_left_edge == 0
+
+  # can only push into transparent parts (empty, furnace, ramp)
+  return false if (not $codeGrid[y][pos_left_edge].transparent? and not $dirty.include?([x, y]))
+
+  # can only push into an empty ramp
+  return false if $codeGrid[y][pos_left_edge] == RampLeft.instance and not pushBlocksLeft(pos_left_edge-1, y-1)
+
+  # do some pushing
+  (pos_left_edge..x-1).each do | xx |
+    $futureCodeGrid[y][xx] = $codeGrid[y][xx+1]
+    $futureCodeGrid[y][xx+1] = Empty.instance
+  end
+  
+  # if we pushed into a furnace, erase the crate and restore the furnace
+  if $codeGrid[y][pos_left_edge] == Furnace.instance
+    $futureCodeGrid[y][pos_left_edge] = Furnace.instance
+  end
+    
+  # if we pushed into a ramp
+  if $codeGrid[y][pos_left_edge] == RampLeft.instance
+    $futureCodeGrid[y-1][pos_left_edge-1] = $futureCodeGrid[y][pos_left_edge]
+    $futureCodeGrid[y][pos_left_edge] = RampLeft.instance
+  end
+
+  return true
+end
+
+def pushBlocksRight(x, y)
+  # can't push if outside the grid
+  return false if y < 0 or y >= 25 or x < 0 or x >= 80
+
+  pos_right_edge = x
+  pos_right_edge += 1 while $codeGrid[y][pos_right_edge].crate? and pos_right_edge < 80-1
+
+  # can't push into the edge of the program
+  return false if pos_right_edge == 80-1
+
+  # can only push into *non-dirty* transparent parts (empty, furnace, ramp)
+  return false if not ($codeGrid[y][pos_right_edge].transparent? and not $dirty.include?([x, y]))
+
+  # can only push into an empty ramp
+  return false if $codeGrid[y][pos_right_edge] == RampRight.instance and not pushBlocksRight(pos_right_edge+1, y-1)
+
+  # do some pushing, marking blockd as dirty
+  (x+1..pos_right_edge).reverse_each do | xx |
+    $dirty << [xx, y]
+    $futureCodeGrid[y][xx] = $codeGrid[y][xx-1]
+    $futureCodeGrid[y][xx-1] = Empty.instance
+  end
+  
+  # if we pushed into a furnace, erase the crate and restore the furnace
+  if $codeGrid[y][pos_right_edge] == Furnace.instance
+    $futureCodeGrid[y][pos_right_edge] = Furnace.instance
+  end
+    
+  # if we pushed into a ramp
+  if $codeGrid[y][pos_right_edge] == RampRight.instance
+    $futureCodeGrid[y-1][pos_right_edge+1] = $futureCodeGrid[y][pos_right_edge]
+    $futureCodeGrid[y][pos_right_edge] = RampRight.instance
+  end
+
+  return true
+end
+
+# Classes
+##############################
+class Part
+  def crate?
+    false
+  end
+
+  def transparent?
+    false
+  end
+
+  def action(x, y)
+    # no default action
+  end
+end
+
+class TurningPoint < Part
+  def char
+    " "
+  end
+end
+
+class Scanner < Part
+  include Singleton
+  def char
+    "*"
+  end
+
+  def category
+    0
+  end
+  
+  def action(x, y)
+    $output << "Scanner action\n"
+  end
+end
+
+class Input < Part
+  include Singleton
+  def char
+    "?"
+  end
+
+  def category
+    0
+  end
+
+  def action(x, y)
+    $output << "Input action\n" 
+ end
+end
+  
+class Furnace < Part
+  include Singleton
+  def transparent?
+    true
+  end
+
+  def char
+    "F"
+  end
+
+  def category
+    1
+  end
+
+  def action(x, y)
+  end
+end
+
+class ConveyorLeft < Part
+  include Singleton
+  def char
+    "<"
+  end
+
+  def category
+    5
+  end
+
+  def action(x, y)
+    if y > 0 and x > 0 and $codeGrid[y-1][x].crate? then
+      pushBlocksLeft(x, y-1)
+    end
+  end
+end
+
+class ConveyorRight < Part
+  include Singleton
+  def char
+    ">"
+  end
+
+  def category
+    5
+  end
+
+  def action(x, y)
+    if y > 0 and x > 0 and $codeGrid[y-1][x].crate? then
+      pushBlocksRight(x, y-1)
+    end
+  end
+end
+
+class RampLeft < Part
+  include Singleton
+  def transparent?
+    true
+  end
+
+  def category
+    3
+  end
+
+  def char
+    '\\'
+  end
+end
+
+class RampRight < Part
+  include Singleton
+  def transparent?
+    true
+  end
+
+  def category
+    3
+  end
+
+  def char
+    '/'
+  end
+end
+
+class Crate < Part
+  attr_accessor :value
+  
+  def crate?
+    true
+  end
+  
+  def initialize
+    @value = 0
+  end
+  
+  def char
+    case @value
+    when (0..9)
+      @value.to_s
+    else
+      "b"
+    end
+  end
+
+  def category
+    3
+  end
+end
+  
+class Empty < Part
+  include Singleton
+  def char
+    " "
+  end
+
+  def transparent?
+    true
+  end
+end
+$empty = Empty.instance
+
+class Wall < Part
+  include Singleton
+  def char
+    "#"
+  end
+end
+$wall = Wall.instance
+
+
+# file must be 80x25
+# lines are padded or cut as necessary
+# blank lines are added at the end if necessary
+
+# reverse the lines, to read the parts and add them to the arrays in evaluation order
+# use the loaded lines otherwise
+
+
+
+def loadChar(char, x, y)
+  entity = $empty
+  data = 0
+
+  case char
+  when 'F'
+    entity = Furnace.instance
+  when '*'
+    entity = Scanner.instance
+  when '<'
+    entity = ConveyorLeft.instance
+  when '>'
+    entity = ConveyorRight.instance
+  when '\\'
+    entity = RampLeft.instance
+  when '/'
+    entity = RampRight.instance
+  when 'b'
+    data = 0
+  when ('0'..'9')
+    entity = Crate.new
+    entity.value = char.to_i
+  when ' '
+    entity = Empty.instance
+  else
+    entity = Wall.instance
+  end
+
+  $codeGrid[y][x] = entity
+  $dataGrid[y][x] = data
+end
+
+def load_grid(code)
+  code.each.with_index do | line, y |
+    line.split('').each.with_index do | char, x |
+      loadChar(char, x, y)
+    end
+  end
+end
+
+def printLevel(code)
+  clear()
+
+  moveto(0, 0)
+  $codeGrid.each.with_index do | line, y |
+    moveto(0, y)
+    print "\x1B[K"
+    line.each.with_index do | part, x |
+      printCode(part.char, x, y)
+    end
+  end
+
+  moveto(0, code.length + 1)
+  print "Output:\n"
+  moveto(0, code.length + 2)
+  u = $output.split("\n").last($outputCount).join("\n")
+  print "\x1B[0J#{u}"
+end
+
+$controlProgram = '+[dsti[o[-]]+]'
+
+def run_one_step(code)
+  # copy the grid into the future one
+  $futureDataGrid = $dataGrid.map do |each| each.dup end
+  $futureCodeGrid = $codeGrid.map do |each| each.dup end
+
+  # (re)create the parts processing order list(s)
+  processing_list = Array.new(6) { Array.new }
+  $codeGrid.each.with_index do | row, y |
+    row.each.with_index do | cell, x |
+      if cell != $empty and cell != $wall then
+        processing_list[cell.category] << [cell, x, y]
+      end
+    end
+  end
+  
+  # activate each part in the proper order
+  processing_list.each do | category |
+    category.each do | entry |
+      entry[0].action(entry[1], entry[2])
+    end
+  end
+
+  # make the future the present
+  $dataGrid = $futureDataGrid
+  $codeGrid = $futureCodeGrid
+end
+
+def run_one_control_cycle(code)
+  # hardcoded +[dsti[o[-]]+] program
+  control = $controlProgram + 'd'
+
+  control.chars.each do | command |
+    case command
+    when "["
+    when "]"
+    when "+"
+    when "-"
+    when ">"
+    when "<"
+    when "d"
+      printLevel(code)
+    when "i"
+    when "o"
+    when "s"
+      sleep($delay)
+    when "t"
+      run_one_step(code)
+    when "a"
+    end
+  end
+
+  $dirty = []
+
+  # input at head (0 if no input) (doesn't block)
+  # if head not zero
+  #   output
+  #   set head to zero
+  # set head to 1
+end
+
+load_grid(code)
+
+(1..20).each do | each |
+  run_one_control_cycle(code)
+end
+
+print "\n"
+exit 1
+
+
+
+
+
+
+# Old MarioLang code for temprorary reference or something
+##########################################################
+loop {
+  oldx = posx
+  oldy = posy
+
+  if posy < 0 then
+    STDERR.print "Error: trying to get out of the program!\n"
+    exit 1
+  end
+
+  if skip == 0 then
+    if should_collect and code[posy][posx] != "'" then
+      vars[varp] = code[posy][posx].ord
+      varp += 1
+      vars << 0 if varp > vars.size - 1
+    else
+      case code[posy][posx]
+      when "'"
+        # varp +=1 if should_collect
+        # vars << 0 if varp > vars.size - 1
+        should_collect = !should_collect
+      when ("0".."9")
+        prefix = prefix * 10 + code[posy][posx].to_i
+      when "\""
+        diry = -1
+        elevator = false
+      when ")"
+        if prefix == 0 then
+          varp += 1
+        else
+          varp += prefix
+        end
+        prefix = 0
+        vars << 0 while varp > vars.size - 1
+      when "("
+        if prefix == 0 then
+          varp -= 1
+        else
+          varp -= prefix
+        end
+        prefix = 0
+        if varp < 0 then
+          STDERR.print "Error: trying to access Memory Cell -1\n"
+          exit 1
+        end
+      when "+"
+        if prefix == 0 then
+          vars[varp] = (vars[varp] + 1) % 256
+        else
+          vars[varp] = (vars[varp] + prefix) % 256
+        end
+        prefix = 0
+      when "-"
+        if prefix == 0 then
+          vars[varp] = (vars[varp] - 1) % 256
+        else
+          vars[varp] = (vars[varp] - prefix) % 256
+        end
+        prefix = 0
+      when "."
+        print vars[varp].chr if not visual
+        $output << vars[varp].chr if visual
+      when ":"
+        print "#{vars[varp]} " if not visual
+        $output << "#{vars[varp]} " if visual
+      when ","
+        vars[varp] = STDIN.getc.ord
+      when ";"
+        vars[varp] = STDIN.gets.to_i
+      when ">"
+        dirx = 1
+      when "<"
+        dirx = -1
+      when "^"
+        diry = -1
+      when "!"
+        dirx = diry = 0
+      when "["
+        skip = 2 if vars[varp] == 0
+      when "@"
+        dirx = -dirx
+      end
+    end
+
+    while code[posy][posx].nil?
+      code[posy] << " "
+    end
+  end
+
+  exit 0 if posy == code.length - 1 or posx >= code[posy+1].length
+
+  if "><@".include?(code[posy][posx]) and skip == 0 then
+    elevator = false
+    diry = 0
+    posx += dirx
+  elsif diry != 0 then
+    skip -= 1 if skip > 0
+    posy += diry
+    diry = 0 if !elevator
+  else
+    case code[posy+1][posx]
+    when "=", "|", "\""
+      posx += dirx
+    when "#"
+      posx += dirx
+      if dirx == 0 and code[posy][posx] == "!" and skip == 0 then
+        elevator = true
+        diry = elevdir(code, posx, posy)
+        if diry == 0 then
+          STDERR.print "Error: No matching elevator ending found!\n"
+          exit 1
+        end
+        posy += diry
+      end
+    else
+      posy += 1
+    end
+    skip -= 1 if skip > 0
+  end
+}
