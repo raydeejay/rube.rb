229fa531c400d75257b259b770e8baaf3b1bb523
Sergi Reyner <sergi.reyner@gmail.com>
2019-04-17 17:02:59 +0100

    Moved AttrBoolean to its own file

--- rube.rb
+++ rube.rb
@@ -10,30 +10,8 @@
 #   (only I'm not actually using Rails, but I'm upping the pun factor here)
 
 require 'singleton'
+require './AttrBoolean'
 require './GetKey'
-
-# Ruby magic
-##############################
-module AttrBoolean
-  def self.included(base)
-    base.extend ClassMethods
-  end
-
-  module ClassMethods
-    def attr_boolean(*names)
-      names.each do |name|
-        define_method(:"#{name}=") do |value|
-          instance_variable_set(:"@#{name}", value)
-        end
-
-        define_method(:"#{name}?") do
-          !!instance_variable_get(:"@#{name}")
-        end
-      end
-    end
-  end
-end
-
 
 # Utility code
 ##############################
