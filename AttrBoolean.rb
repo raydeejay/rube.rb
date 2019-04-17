1cbe6f371dd425d3b953c48a759b03ef93e037cb
Sergi Reyner <sergi.reyner@gmail.com>
2019-04-17 23:59:55 +0100

    Enhanced AttrBoolean to provide getters, setters, and accessors

--- AttrBoolean.rb
+++ AttrBoolean.rb
@@ -5,14 +5,19 @@ module AttrBoolean
 
   module ClassMethods
     def attr_boolean(*names)
+      attr_boolean_writer(*names)
+      attr_boolean_reader(*names)
+    end
+    def attr_boolean_reader(*names)
       names.each do |name|
-        define_method(:"#{name}=") do |value|
-          instance_variable_set(:"@#{name}", value)
-        end
-
         define_method(:"#{name}?") do
           !!instance_variable_get(:"@#{name}")
         end
+      end
+    end
+    def attr_boolean_writer(*names)
+      define_method(:"#{name}=") do |value|
+        instance_variable_set(:"@#{name}", value)
       end
     end
   end
