6a2eff7a4c3475e5a43e8449a2af2d264b354296
Sergi Reyner <sergi.reyner@gmail.com>
2019-04-21 00:37:46 +0100

    Modify AttrBoolean to be self or nil

--- AttrBoolean.rb
+++ AttrBoolean.rb
@@ -8,17 +8,29 @@ module AttrBoolean
       attr_boolean_writer(*names)
       attr_boolean_reader(*names)
     end
+    # new implementation returning the value or nil
     def attr_boolean_reader(*names)
       names.each do |name|
         define_method(:"#{name}?") do
-          !!instance_variable_get(:"@#{name}")
+          it = instance_variable_get(:"@#{name}")
+          it ? it : nil
         end
       end
     end
+    # original implementation returning booleans
+
+    #   def attr_boolean_reader(*names)
+    #     names.each do |name|
+    #       define_method(:"#{name}?") do
+    #         !!instance_variable_get(:"@#{name}")
+    #       end
+    #     end
+    #   end
     def attr_boolean_writer(*names)
       define_method(:"#{name}=") do |value|
         instance_variable_set(:"@#{name}", value)
       end
     end
   end
+
 end
